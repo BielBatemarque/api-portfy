b53c3aa269e24f7d5b590b25d234f99637e87900
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2025-11-10 07:08:17 -0300

    feat: implentando conexão com banco de dados postgreeSQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: 5430,
+    database: process.env.POSTGRES_DB,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    autoLoadEntities: process.env.SERVER === "DEV", 
+    synchronize: process.env.SERVER === "DEV"
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
